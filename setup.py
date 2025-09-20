2466dddd078437192d6c05b2cfed05a930363c85
thakuranubhav <thakuranubhav867@gamil.com>
2025-09-20 22:23:55 +0530

    third commit

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='mlproject',
     version='0.0.1',
-    author="Krish",
+    author="anubhav",
     author_email='thakuranubhav867@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
